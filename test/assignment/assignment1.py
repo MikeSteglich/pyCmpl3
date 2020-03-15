e4a40511f2fdb19e5c523b36e2bce36f9de351f7
Mike Steglich <mike.steglich@th-wildau.de>
2020-03-15 15:17:41 +0100

    test files

---
+++ test/assignment/assignment1.py
@@ -0,0 +1,44 @@
+#!/usr/bin/python 
+
+from pyCmpl import *
+
+try: 
+	m = Cmpl("assignment.cmpl")
+	
+	locations = CmplSet("locations")
+	locations.setValues(1,4)
+	
+	machines = CmplSet("machines")
+	machines.setValues(1,3)
+	
+	combinations = CmplSet("A", 2)
+	combinations.setValues([ [1,1],[1,2],[1,3],[1,4], [2,1],[2,3],[2,4], [3,1],[3,2],[3,3],[3,4]])
+		
+	c = CmplParameter("c",combinations)
+	c.setValues([13,16,12,11,15,13,20,5,7,10,6])
+	
+	m.setSets(machines,locations,combinations)
+	m.setParameters(c)
+	#m.debug()
+	m.setOutput(True)
+	#m.connect("http://127.0.0.1:8008")
+	
+	m.solve()
+
+	print(("Objective value: " , m.solution.value))
+	print(("Objective status: " , m.solution.status))
+	
+	print("Variables:")
+	for v in m.solution.variables:
+		print(("%10s %3s %8i %8i %8i" % (v.name, v.type, v.activity,v.lowerBound,v.upperBound  )))
+	
+	print("Constraints:") 
+	for c in m.solution.constraints:
+		print(("%10s %3s %8.0f %8.0f %8.0f" % (c.name, c.type, c.activity,c.lowerBound,c.upperBound)))
+
+	m.solutionReport()
+
+except CmplException as e:
+	print((e.msg))
+	
+	
---
+++ test/cutting-stock/mod1.mps
@@ -0,0 +1,23 @@
+* CMPL - Free-MPS - Export
+NAME          cmpl__cjee_6n2__cut-pattern.cmpl
+ROWS
+ N line[1]
+ L line[2]
+COLUMNS
+ GVANF 'MARKER' 'INTORG'
+ use[20] line[1] 0.200000 line[2] 20
+ use[45] line[1] 0.500000 line[2] 45
+ use[50] line[1] 0.500000 line[2] 50
+ use[55] line[1] 0.500000 line[2] 55
+ use[75] line[1] 1.000000 line[2] 75
+ GVEND 'MARKER' 'INTEND'
+RHS
+ RHS line[2] 110
+RANGES
+BOUNDS
+ PL BOUND use[20]
+ PL BOUND use[45]
+ PL BOUND use[50]
+ PL BOUND use[55]
+ PL BOUND use[75]
+ENDATA
